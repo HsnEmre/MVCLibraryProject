6d2550c113aad64b0c5c183791617078c2314d26
Hasan Emre Buzkan <hasanemre.b@hotmail.com>
2024-05-13 17:04:22 +0300

    Loan

---
+++ MVCLibrary/Controllers/LoanController.cs
@@ -0,0 +1,41 @@
+﻿using MVCLibrary.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCLibrary.Controllers
+{
+    public class LoanController : Controller
+    {
+        #region db
+        DBLIBRARYEntities db=new DBLIBRARYEntities();
+        #endregion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region Loan
+        [HttpGet]
+        public ActionResult Loan()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public ActionResult Loan(TBLMOVE paraeter)
+        {
+            db.TBLMOVE.Add(paraeter);
+            db.SaveChanges();
+            return View();
+        }
+
+        #endregion
+
+        #region MyRegion
+
+        #endregion
+    }
+}
